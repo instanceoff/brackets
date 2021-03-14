d0cf71d197982162194fa8e3076ff8d384233c4e
Ilya Khinevich <ilyaxinevich@gmail.com>
2021-03-14 15:24:18 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    const brack = bracketsConfig[i].join('');
+    if (str.includes(brack)){
+      str = str.replace(brack, '');
+      i = -1;
+    }
+  }
+  return !str;
 }
